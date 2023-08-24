d8b07af6d448724f2d4003befeb1497c9d2baf43
Mark Njoroge <marknjo7@gmail.com>
2023-08-24 08:07:08 +0300

    update Favorite form to use fetcher instead of Form component

--- app/components/Favorite.tsx
+++ app/components/Favorite.tsx
@@ -1,4 +1,4 @@
-import { Form } from '@remix-run/react';
+import { useFetcher } from '@remix-run/react';
 import type { FunctionComponent } from 'react';
 import type { ContactRecord } from '~/data';
 
@@ -6,16 +6,17 @@ const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, 'favorite'>;
 }> = ({ contact }) => {
   const favorite = contact.favorite;
+  const fetcher = useFetcher();
 
   return (
-    <Form method='post'>
+    <fetcher.Form method='post'>
       <button
         aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
         name='favorite'
         value={favorite ? 'false' : 'true'}>
         {favorite ? '★' : '☆'}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 };
 
